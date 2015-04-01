9dfc81296654e5a83c0226de9b270a98bdb14dff
Corey Leveen <mntj11@gmail.com>
2015-03-31 22:30:43 -0400

    Begin buildout of talkbackbot_plugin

--- talkback/twisted/plugins/talkbackbot_plugin.py
+++ talkback/twisted/plugins/talkbackbot_plugin.py
@@ -8,3 +8,26 @@ from zope.interface import implementer
 
 from talkback.bot import TalkBackBotFactory
 from talkback.quote_picker import QuotePicker
+
+class Options(usage.Options):
+
+class TalkBackBotService(Service):
+
+    def __init__(self, endpoint, channel, nickname, realname, quotesFilename, triggers):
+
+    def startService(self):
+        """Construct a client & connect to server."""
+
+    def stopService(self):
+        """Disconnect."""
+
+class BotServiceMaker(object):
+    tapname = "twsrs"
+    description = "IRC bot that provides quotations from notable women"
+    options = Options
+
+    def makeService(self, options):
+        """Construct the talkbackbot service."""
+
+
+serviceMaker = BotServiceMaker()
